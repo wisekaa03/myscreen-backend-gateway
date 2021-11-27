843e0d9f207b78c6496d1d5dda63be8adcb7c864
Stanislav Vyaliy <wisekaa03@gmail.com>
2021-11-28 02:21:08 +0300

    cosmetic fix

---
+++ src/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './errors';
+export * from './request';
+export * from './response';
+
+export * from './status.enum';
+export * from './user.dto';
--- src/endpoint/dto/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@/dto/errors';
-export * from '@/dto/request';
-export * from '@/dto/response';
-
-export * from '@/dto/status.enum';
-export * from '@/dto/user.dto';
