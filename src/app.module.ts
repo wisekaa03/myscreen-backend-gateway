59c3eeb80e2cff0393dd9ef155b33d23935924d0
Stanislav Vyaliy <wisekaa03@gmail.com>
2022-02-11 21:06:17 +0300

    logger -> elasticsearch

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,9 @@ import { Module, Logger } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { S3Module } from 'nestjs-s3';
 import type pino from 'pino';
-import { LoggerModule } from 'nestjs-pino';
+import type { PrettyOptions } from 'pino-pretty';
+import type { ClientOptions as ElasticClientOptions } from '@elastic/elasticsearch';
+import { LoggerModule, Params as NestPinoParams } from 'nestjs-pino';
 
 import { S3ModuleOptionsClass } from '@/shared/s3-module-options-class';
 import { MailModule } from '@/mail/mail.module';
@@ -17,32 +19,53 @@ import { WSModule } from '@/websocket/ws.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     LoggerModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        pinoHttp: {
-          level: configService.get<pino.LevelWithSilent>('LOG_LEVEL', 'debug'),
-          transport: {
-            targets: [
-              {
-                target:
-                  process.env.NODE_ENV === 'test'
-                    ? 'pino-pretty'
-                    : `${__dirname}/shared/pino-pretty.js`,
-                options: {
-                  colorize: process.env.NODE_ENV !== 'production',
-                  translateTime: 'yyyy-mm-dd HH:MM:ss',
-                  singleLine: true,
-                  ignore: 'pid,hostname',
-                },
-                level: configService.get<pino.LevelWithSilent>(
-                  'LOG_LEVEL',
-                  'debug',
-                ),
-              },
-              // TODO: Elasticsearch...
-            ],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<NestPinoParams> => {
+        const targets: pino.TransportTargetOptions[] = [];
+
+        // Pretty-print
+        const prettyPrint: pino.TransportTargetOptions<PrettyOptions> = {
+          target:
+            process.env.NODE_ENV === 'test'
+              ? 'pino-pretty'
+              : `${__dirname}/shared/pino-pretty.js`,
+          options: {
+            colorize: process.env.NODE_ENV !== 'production',
+            translateTime: 'yyyy-mm-dd HH:MM:ss',
+            singleLine: true,
+            ignore: 'pid,hostname',
           },
-        },
-      }),
+          level: configService.get<pino.LevelWithSilent>('LOG_LEVEL', 'debug'),
+        };
+        targets.push(prettyPrint);
+
+        // ElasticSearch
+        const kibanaHost = configService.get<string>('KIBANA_HOST');
+        if (kibanaHost) {
+          const kibana: pino.TransportTargetOptions<ElasticClientOptions> = {
+            target: 'pino-elasticsearch',
+            options: {
+              // TODO
+            },
+            level: configService.get<pino.LevelWithSilent>(
+              'LOG_LEVEL',
+              'debug',
+            ),
+          };
+          targets.push(kibana);
+        }
+
+        return {
+          pinoHttp: {
+            level: configService.get<pino.LevelWithSilent>(
+              'LOG_LEVEL',
+              'debug',
+            ),
+            transport: { targets },
+          },
+        };
+      },
       inject: [ConfigService],
     }),
 
