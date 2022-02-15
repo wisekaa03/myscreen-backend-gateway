8f721f5b9a71578d435c624a66381a3c19d47eee
Stanislav Vyaliy <wisekaa03@gmail.com>
2022-02-15 17:36:41 +0300

    pm2 fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'api',
-      script: './dist/src/main.js',
+      script: 'xvfb-run node ./dist/src/main.js',
       max_memory_restart: '7000M',
       env: {
         NODE_ENV: 'production',
