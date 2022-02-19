76229b8641dccf10a84e848f3c9e44bca9082fa7
Stanislav Vyaliy <wisekaa03@gmail.com>
2022-02-20 00:44:02 +0300

    package bump

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,10 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
 
+import { WsAdapter } from '@/websocket/ws-adapter';
 import { version, author, homepage, description } from '../package.json';
 import { AppModule } from './app.module';
 import { ExceptionsFilter } from './exception/exceptions.filter';
-import { WsAdapter } from '@/websocket/ws-adapter';
 
 (async () => {
   const configService = new ConfigService();
