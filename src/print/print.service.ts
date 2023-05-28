17c4303d33907470bc2cafc1f012e2129a4b44c2
Stanislav V Vyaliy <stanislav@wisekaa.dev>
2023-05-28 18:30:08 +0300

    invoice fix

--- src/print/print.service.ts
+++ src/print/print.service.ts
@@ -24,8 +24,163 @@ export class PrintService {
     const options: XLSX.WritingOptions = { bookType: 'xlsx', type: 'buffer' };
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet<CellObject>([
-      [{ v: 'Счёт', t: 's' }],
+      [
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        {
+          v: 'Адрес: 119021, Россия, г. Москва, ул. Льва Толстого, д. 16',
+          t: 's',
+          s: { alignment: { horizontal: 'right' }, font: { bold: true } },
+        },
+      ],
+      [
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        {
+          v: 'тел. (495) 739-70-00',
+          t: 's',
+          s: { font: { bold: true }, alignment: { horizontal: 'right' } },
+        },
+      ],
+      [
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        {
+          v: 'факс. (495) 739-70-70',
+          t: 's',
+          s: { font: { bold: true }, alignment: { horizontal: 'right' } },
+        },
+      ],
+      [],
+      [
+        { v: '', t: 's' },
+        {
+          v: 'Образец заполнения платежного поручения',
+          t: 's',
+          s: { font: { bold: true }, alignment: { horizontal: 'center' } },
+        },
+      ],
+      [],
+      [
+        { v: '', t: 's' },
+        { v: 'Получатель', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: 'Сч. №', t: 's', s: { alignment: { vertical: 'top' } } },
+        {
+          v: '40702810600014307627',
+          t: 's',
+          s: { alignment: { vertical: 'top' } },
+        },
+      ],
+      [
+        { v: '', t: 's' },
+        { v: 'ИНН/КПП 7736207543/997750001 ООО "ЯНДЕКС"', t: 's' },
+      ],
+      [
+        { v: '', t: 's' },
+        { v: 'Банк получателя', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: 'БИК', t: 's' },
+        { v: '44525545', t: 's' },
+      ],
+      [
+        { v: '', t: 's' },
+        {
+          v: 'АО Юникредит Банк, 119034, г. Москва, Пречистенская наб., д. 9 ',
+          t: 's',
+          s: { alignment: { wrapText: true } },
+        },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: 'Сч. №', t: 's', s: { alignment: { vertical: 'top' } } },
+        {
+          v: '30101810300000000000000',
+          t: 's',
+          s: { alignment: { vertical: 'top' } },
+        },
+      ],
+      [],
+      [
+        { v: '', t: 's' },
+        {
+          v: 'Условия для расчетов',
+          t: 's',
+          s: { font: { bold: true, underline: true } },
+        },
+      ],
+      [],
+      [
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        { v: '1. Счет действителен в течение пяти банковских дней.', t: 's' },
+      ],
+      [
+        { v: '', t: 's' },
+        { v: '', t: 's' },
+        {
+          v: '2. В назначении платежа, пожалуйста, указывайте номер счета.',
+          t: 's',
+        },
+      ],
+      [],
+      [
+        { v: '', t: 's' },
+        {
+          v: 'СЧЕТ № Б-2438137758-1 от 27 февраля 2020 г.',
+          t: 's',
+          s: { font: { bold: true }, alignment: { horizontal: 'center' } },
+        },
+      ],
+      [],
     ]);
+    ws['!cols'] = [
+      { wch: 5 },
+      { wch: 5 },
+      { wch: 19 },
+      { wch: 11 },
+      { wch: 15 },
+      { wch: 11 },
+      { wch: 11 },
+      { wch: 15 },
+    ];
+    ws['!rows'] = [{}, {}, {}, {}, {}, {}, {}, {}, {}, { hpx: 24 }];
+    ws['!merges'] = [
+      { s: { r: 4, c: 1 }, e: { r: 4, c: 7 } },
+
+      { s: { r: 6, c: 1 }, e: { r: 6, c: 4 } },
+      { s: { r: 6, c: 5 }, e: { r: 7, c: 5 } },
+      { s: { r: 6, c: 6 }, e: { r: 7, c: 7 } },
+
+      { s: { r: 7, c: 1 }, e: { r: 7, c: 4 } },
+
+      { s: { r: 8, c: 1 }, e: { r: 8, c: 4 } },
+      { s: { r: 8, c: 6 }, e: { r: 8, c: 7 } },
+
+      { s: { r: 9, c: 1 }, e: { r: 9, c: 4 } },
+      { s: { r: 9, c: 6 }, e: { r: 9, c: 7 } },
+
+      { s: { r: 16, c: 1 }, e: { r: 16, c: 7 } },
+    ];
     XLSX.utils.book_append_sheet(wb, ws, 'Счёт');
 
     return XLSX.writeXLSX(wb, options);
