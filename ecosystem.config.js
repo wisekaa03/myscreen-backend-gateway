c49fe501e9506be9bc4b995e0efdc1a976f61bc9
Stanislav V Vyaliy <stanislav@wisekaa.dev>
2023-12-12 13:44:27 +0300

    dependecy upgrade

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'api',
       script: 'xvfb-run node ./dist/src/main.js',
-      max_memory_restart: '7000M',
+      max_memory_restart: '1000M',
       env: {
         NODE_ENV: 'production',
       },
