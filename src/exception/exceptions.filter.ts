3f4373b6d667d51f4bfbd9c2e44eb24f9fffba14
Stanislav Lanskoy <stanislav@wisekaa.dev>
2024-10-29 18:19:31 +0300

    i18n fix

--- src/exception/exceptions.filter.ts
+++ src/exception/exceptions.filter.ts
@@ -34,7 +34,7 @@ export class ExceptionsFilter extends BaseExceptionFilter<Error> {
   }
 
   catch(exception: HttpException | Error, host: ArgumentsHost) {
-    const i18n = I18nContext.current(host);
+    const i18n = I18nContext?.current(host);
     let { message } = exception;
 
     if (exception instanceof I18nValidationException) {
